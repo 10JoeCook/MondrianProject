68727c73ad7625356f337bfb0a4aedc111b959ee
10JoeCook <10joe.cook@gmail.com>
2021-08-23 21:34:13 +0100

    RecursiveRectangleIllustrator.py -import random -added run and draw_img() functions to provide basic functionality

--- Illustrators/RecursiveRectangleIllustrator.py
+++ Illustrators/RecursiveRectangleIllustrator.py
@@ -1,5 +1,6 @@
 from Generators import RecursiveRectangle
 from tkinter import Tk, Canvas
+import random
 
 
 class RecursiveRectangleIllustrator:
@@ -9,6 +10,16 @@ class RecursiveRectangleIllustrator:
 		self.window = Tk()
 		self.canvas = Canvas(self.window, width=self.rect.get_width(), height=self.rect.get_height())
 		self.canvas.pack()
-		self.canvas.bind("<Button-1>", self.mouse_click)
+		# self.canvas.bind("<Button-1>", self.mouse_click)
 		self.draw_img()
 		self.run()
+
+	def run(self):
+		self.window.mainloop()
+
+	def draw_img(self):
+		rects = self.rect.collect_rects()
+		for rect in rects:
+			self.canvas.create_rectangle(rect.x - rect.half_width, rect.y - rect.half_height,
+										rect.x + rect.half_width, rect.y + rect.half_height,
+										fill=rect.color, width=random.randint(rect.bar_width_lower, rect.bar_width_upper))
