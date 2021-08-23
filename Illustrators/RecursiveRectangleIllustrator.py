ce88d26e6ad23ea1af1e125bc3fd922bc5458cad
10JoeCook <10joe.cook@gmail.com>
2021-08-23 21:38:03 +0100

    RecursiveRectangleIllustrator.py -fixed imports -added draw_new_img() and mouseclick functions to allow for new images to be generated durning runtime

--- Illustrators/RecursiveRectangleIllustrator.py
+++ Illustrators/RecursiveRectangleIllustrator.py
@@ -1,4 +1,4 @@
-from Generators import RecursiveRectangle
+from Generators.RecursiveRectangle import RecursiveRectangle
 from tkinter import Tk, Canvas
 import random
 
@@ -10,7 +10,7 @@ class RecursiveRectangleIllustrator:
 		self.window = Tk()
 		self.canvas = Canvas(self.window, width=self.rect.get_width(), height=self.rect.get_height())
 		self.canvas.pack()
-		# self.canvas.bind("<Button-1>", self.mouse_click)
+		self.canvas.bind("<Button-1>", self.mouse_click)
 		self.draw_img()
 		self.run()
 
@@ -23,3 +23,13 @@ class RecursiveRectangleIllustrator:
 			self.canvas.create_rectangle(rect.x - rect.half_width, rect.y - rect.half_height,
 										rect.x + rect.half_width, rect.y + rect.half_height,
 										fill=rect.color, width=random.randint(rect.bar_width_lower, rect.bar_width_upper))
+
+	def draw_new_img(self):
+		self.rect = RecursiveRectangle((self.rect.x, self.rect.y), (self.rect.half_width, self.rect.half_height), colors=self.rect.possible_colors)
+		self.rect.generate()
+		self.canvas.delete("all")
+		self.draw_img()
+		self.save_no += 1
+
+	def mouse_click(self, event):
+		self.draw_new_img()
