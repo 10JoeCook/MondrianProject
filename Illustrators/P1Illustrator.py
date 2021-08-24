25177c375db887c37561cf5550a8f393bf119ca3
10JoeCook <10joe.cook@gmail.com>
2021-08-24 16:51:05 +0100

    P1Illustrator.py -minor style change -added the drawing of lines to the draw img function

--- Illustrators/P1Illustrator.py
+++ Illustrators/P1Illustrator.py
@@ -16,8 +16,7 @@ class P1Illustrator:
 		self.window.mainloop()
 
 	def draw_img(self):
-		rects = self.img.get_rects()
-		for rect in rects:
+		for rect in self.img.get_rects():
 			if rect[2] == 1:
 				color = "#FFFFFF"
 			elif rect[2] == 2:
@@ -29,4 +28,7 @@ class P1Illustrator:
 			else:
 				color = "#000000"
 			self.canvas.create_rectangle(rect[0][0], rect[0][1], rect[1][0], rect[1][1],
-			                             fill=color)
+			                             fill=color, width=0)
+		for line in self.img.get_lines():
+			self.canvas.create_line(line[0][0], line[0][1], line[1][0], line[1][1],
+			                        fill="#000000", width=line[2])
