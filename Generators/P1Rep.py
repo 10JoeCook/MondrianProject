7e828774bc9f1db34f96700a88eefca57b79f5e3
10JoeCook <10joe.cook@gmail.com>
2021-08-24 14:29:04 +0100

    P1Rep.py -defined a MondrianImg class to represent a mondrian painting using the representation described in Andrzejewski et al

---
+++ Generators/P1Rep.py
@@ -0,0 +1,14 @@
+class MondrianImg:
+	def __init__(self, ymax: int, xmax:int, vpts: list, hpts: list, rect_colors: list, vext: list, vthick: list,
+	             hext: list, hthick: list, rects: list):
+		self.y_max = ymax
+		self.x_max = xmax
+		self.v_pts = vpts
+		self.h_pts = hpts
+		self.rect_colours = rect_colors
+		self.v_ext = vext
+		self.v_thick = vthick
+		self.h_ext = hext
+		self.h_thick = hthick
+		self.rects = rects
+
