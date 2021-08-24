924c3b33eeec38167bcc065f54fbaabda9e99889
10JoeCook <10joe.cook@gmail.com>
2021-08-24 16:49:45 +0100

    P1Illustrator.py -added the get_lines() function     -returns the lines in a format useful for tkinter

--- Generators/P1Rep.py
+++ Generators/P1Rep.py
@@ -15,8 +15,22 @@ class MondrianImg:
 	def get_rects(self) -> list:
 		ret = []
 		for rect in self.rects:
-			tl = (self.v_pts[rect[0]], self.h_pts[rect[2]])
-			br = (self.v_pts[rect[1]], self.h_pts[rect[3]])
+			tl = (self.v_pts[rect[0] - 1], self.h_pts[rect[2] - 1])
+			br = (self.v_pts[rect[1] - 1], self.h_pts[rect[3] - 1])
 			color = self.rect_colours[self.rects.index(rect)]
 			ret.append((tl, br, color))
+		return ret
+
+	def get_lines(self) -> list:
+		ret = []
+		for i in range(len(self.v_pts)):
+			start = (self.v_pts[i], self.h_pts[self.v_ext[i][0] - 1])
+			end = (self.v_pts[i], self.h_pts[self.v_ext[i][1] - 1])
+			thickness = self.v_thick[i]
+			ret.append((start, end, thickness))
+		for i in range(len(self.h_pts)):
+			start = (self.v_pts[self.h_ext[i][0] - 1], self.h_pts[i])
+			end = (self.v_pts[self.h_ext[i][1] - 1], self.h_pts[i])
+			thickness = self.h_thick[i]
+			ret.append((start, end, thickness))
 		return ret
