da9b4cff1b4d1df8de00bf8a705d4cf8adec8f72
10JoeCook <10joe.cook@gmail.com>
2021-08-24 23:18:12 +0100

    Readers.py -Added code to generate lists of each type of attribute to be used for analysis

--- Utils/Readers.py
+++ Utils/Readers.py
@@ -13,6 +13,7 @@ class MatReader:
 		self.names = []
 		self.reps = []
 		self.images = []
+		self.attr_lists = {}
 
 	def read(self):
 		self.file_contents = scipy.io.loadmat(self.filename)
@@ -63,6 +64,45 @@ class MatReader:
 			                               hext,
 			                               hthick,
 			                               rects))
+
+	def generate_attr_lists(self):
+		self.attr_lists["ymax"] = []
+		self.attr_lists["xmax"] = []
+		self.attr_lists["vpts"] = []
+		self.attr_lists["vext"] = []
+		self.attr_lists["vthick"] = []
+		self.attr_lists["htps"] = []
+		self.attr_lists["hext"] = []
+		self.attr_lists["hthick"] = []
+		self.attr_lists["rects"] = []
+		self.attr_lists["rect_colors"] = []
+		for i in range(len(self.names)):
+			rep = self.reps[i]
+			self.attr_lists["ymax"].append(int(rep[0][0][0]))
+			self.attr_lists["xmax"].append(int(rep[1][0][0]))
+			self.attr_lists["vpts"].append(list(rep[2][0]))
+			vext = list(rep[3])
+			for i in range(len(vext)):
+				vext[i] = list(vext[i])
+			self.attr_lists["vext"].append(vext)
+			vthick = list(rep[4])
+			for i in range(len(vthick)):
+				vthick[i] = list(vthick[i])
+			self.attr_lists["vthick"].append(vthick)
+			self.attr_lists["hpts"].appemd(list(rep[5][0]))
+			hext = list(rep[6])
+			for i in range(len(hext)):
+				hext[i] = list(hext[i])
+			self.attr_lists["hext"].append(hext)
+			hthick = list(rep[7])
+			for i in range(len(hthick)):
+				hthick[i] = list(hthick[i])
+			self.attr_lists["hthick"].append(hthick)
+			rects = list(rep[8])
+			for i in range(len(rects)):
+				rects[i] = list(rects[i])
+			self.attr_lists["rects"].append(rects)
+			self.attr_lists["rect_colors"].append(list(rep[9][0]))
 
 	def get_images(self):
 		return self.images
