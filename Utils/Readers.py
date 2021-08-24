adcb5f7a18021f1fecd94d8888b0668e96733b83
10JoeCook <10joe.cook@gmail.com>
2021-08-24 21:53:14 +0100

    Readers.py -import MondrainImg -implmentation of generate_image_objects() -TODO: include name in img object

--- Utils/Readers.py
+++ Utils/Readers.py
@@ -1,4 +1,5 @@
 import scipy.io
+from Generators.P1Rep import MondrianImg
 
 
 class MatReader:
@@ -29,4 +30,40 @@ class MatReader:
 		self.generate_image_objects()
 
 	def generate_image_objects(self):
-		pass
+		for i in range(len(self.names)):
+			name = self.names[i]
+			rep = self.reps[i]
+			ymax = int(rep[0][0][0])
+			xmax = int(rep[1][0][0])
+			vpts = list(rep[2][0])
+			vext = list(rep[3])
+			for i in range(len(vext)):
+				vext[i] = list(vext[i])
+			vthick = list(rep[4])
+			for i in range(len(vthick)):
+				vthick[i] = list(vthick[i])
+			hpts = list(rep[5][0])
+			hext = list(rep[6])
+			for i in range(len(hext)):
+				hext[i] = list(hext[i])
+			hthick = list(rep[7])
+			for i in range(len(hthick)):
+				hthick[i] = list(hthick[i])
+			rects = list(rep[8])
+			for i in range(len(rects)):
+				rects[i] = list(rects[i])
+			rect_colors = list(rep[9][0])
+			self.images.append(MondrianImg(ymax,
+			                               xmax,
+			                               vpts,
+			                               hpts,
+			                               rect_colors,
+			                               vext,
+			                               vthick,
+			                               hext,
+			                               hthick,
+			                               rects))
+
+	def get_images(self):
+		return self.images
+
