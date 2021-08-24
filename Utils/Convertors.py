9a4d74ebf2a40be6718273bc07b5ffebd1cca01f
10JoeCook <10joe.cook@gmail.com>
2021-08-24 20:02:24 +0100

    Convertors.py -added __str__ to print mat files -added pass to convert function as it is not yet implemented

--- Utils/Convertors.py
+++ Utils/Convertors.py
@@ -6,8 +6,15 @@ class MatToTxt:
 		self.filename = filename
 		self.file_contents = None
 
+	def __str__(self):
+		out = ""
+		for val in self.file_contents.keys():
+			print(val)
+			print(self.file_contents[val])
+
 	def read(self):
 		self.file_contents = scipy.io.loadmat(self.filename)
 
 	def convert(self):
-		print(self.file_contents.keys())
+		pass
+
